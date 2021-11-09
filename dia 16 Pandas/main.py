129bca8c03d9f8f073c34689ce4b2ecd48ea1f5a
Ramon <ramonnicolasbeiler@gmail.com>
2021-11-08 21:28:37 -0300

    pandas

--- dia 16 Pandas/main.py
+++ dia 16 Pandas/main.py
@@ -38,3 +38,8 @@ dict_new = {"students":["AMY","sissok","Angela"],
 df = pd.DataFrame(dict_new)
 df.to_csv("novo_csv.csv")
 print(df)
+
+squirrel = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+squirrel_hectare = squirrel.Hectare
+make=pd.DataFrame(squirrel_hectare)
+
